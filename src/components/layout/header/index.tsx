4c912015274c31c190e45a170b1f1424304dcafd
kimmeh <mhko0411@gmail.com>
2023-05-31 22:00:25 +0900

    header 추가

---
+++ src/components/layout/header/index.tsx
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+import { HEADER_HEIGHT } from "@/styles/constant";
+
+const Container = styled.div`
+  width: 100%;
+  height: ${HEADER_HEIGHT}px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  box-shadow: 0 1px 7px #e6e6e6;
+`;
+
+const Title = styled.h1`
+  font-size: 42px;
+  font-weight: 700;
+  color: ${(props) => props.theme.colors.main};
+`;
+
+const Header = () => {
+  return (
+    <Container>
+      <Title>codewoods</Title>
+    </Container>
+  );
+};
+
+export default Header;
---
+++ src/styles/theme.ts
@@ -0,0 +1,8 @@
+const theme = {
+  colors: {
+    main: "#2D862D",
+    light: "8CD98C",
+  },
+};
+
+export default theme;
