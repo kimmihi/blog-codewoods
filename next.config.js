b2b5079c8a6c123d2954d9a003aece513d50a87a
kimmeh <mhko0411@gmail.com>
2023-08-09 21:58:31 +0900

    add homepage

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/",
+  basePath: "https://kimmihi.github.io/blog-codewoods/",
   reactStrictMode: true,
   compiler: {
     styledComponents: true,
